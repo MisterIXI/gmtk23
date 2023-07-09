dc6941a72f65be6e524bf83818551e1176f34d3f
hyuroki <mf.gangsta.thebest@gmail.com>
2023-07-09 15:58:43 +0200

    Twitch Chat done

--- Gamerrage/Assets/TwatchChat.cs
+++ Gamerrage/Assets/TwatchChat.cs
@@ -11,8 +11,8 @@ public class TwatchChat : MonoBehaviour
     float nextMessageTime = 0.5f;
     public int MaxMessages = 40;
 
-    string[] colors = {"red"};
-    string[] usernames = {"sazzles"};
+    string[] colors = {"#00ffffff","#0000ffff","#ff00ffff", "#008000ff", "#00ff00ff", "#ffa500ff", "#ff0000ff", "#ffff00ff"};
+    string[] usernames = {"sazzles","nuttyskateboard","crashanalyst","weirdzing","prizewhirr","doog","doggo6","catmash","batman56","zeroi","tweetcurly","evebrewer","oinklung","forecastser","MisterIXI","sazzles","Hyuroki","countercurl","Sherpod","Lollo","Wayai","SupremePat3","Freexis","Robyes","Tarall45","1337Hax0r","Nippy","Tnt23","N1ifgt","892fgh","4rchik1l","Jumbl3","W4dfhe","Mirror32","Luff1","catnip2","roflcorny","ItIsYe","MrGame","fifilikescats","islathetequila","beefy74","beb1","Dirk","nick3212","username34","boi89","superk1ll3r","piraz","Olgnfar","Omu_sa","kilwol1","pandaman237","manu8l","piggatsu","palmtree61","gemer","ninjaboi8","xluvX","david_lynt","50knife51","Damni","Search55","y4nsi","Splinter324","RealKoll3","h4X0Rm3an","jackiii112","excuton","lilalulo","m0m3yx","ferryas","asdfghjiko","1118ee112","hecke89","brettchef","satakeluv","t00ts111","jonesie","reddog","catnip1","katti7","edgelord99","kawaiihawaii","nippitwist","mnvasdf","sanfterbube2","saftnase","sweet11","1337_AMA_L","LLAMA4","letter_box","breadcat","salam1","indioana_dog","harambes_wife","number_8ight","laxsaxmax","1892klover","piano1234","tigerwutz","spongey1","bunnydad","omegalovan1","sauna0","squeez"};
     string[] neutral = {"hi","Hello","Hi!","Yooo","lmao","haha","lol","LUL","ur muted","ads wtf","hahaha",":D","is this live?","whats this game called","same lol","how are you today","can we play something else","why are we playing this","no way","yoo","what are these graphics","is this ranked?","<3","yooo","XD","xd","uhh","oh","yo","waddup","im back what did i miss",":)","oh man","classic cha0sgamer haha","lol","dude","this looks like trash tbh","that splatanim xD","greetings from germany","8)",":3","im hungry","OMEGALUL"};
     string[] excited = {"OH","pausing","almost there","Pausechamp","YOU CAN DO IT","Pausechamp","Pausechamp","Pausechamp","Pausechamp","Pausechamp","Puasechamp","pausing"};
     string[] fail = {"KEKW","HAHAHAHHAHAHAHa","SO BAD","LOLOLL","HAHAHHAA","HAHAAHA","WASHED UP","NA jump","xDDD","XDDDD","LMAAOOO","soooBAD","LUL","OMEGALUL","HAAHAHA","SOOO BAD","KEKW KEKW KEKW KEKW","KEK","KEKW KEKW KEKW","KEKW KEKW","KEKW","KEKW","KEKW","KEKW","KEKW KEKW","KEK","KEK","LOLOOL","XDDD","AHAHAA","HAHAA","OMEGALUL","OMEGALUL","OMEGALUL","OMEGALUL"};
@@ -39,8 +39,29 @@ public class TwatchChat : MonoBehaviour
             int excitementLvl = getExcitement();
             string username = usernames[(int)Random.Range(0f,usernames.Length)];
             string[] msg = MsgDictionary[excitementLvl];
-            SendTwatchMessage(username, msg[(int)Random.Range(0f,(int)msg.Length)]);
-            nextMessageTime = Time.time + 2f;
+            int randomizer = (int) Random.Range(1f,10f);
+            switch(randomizer){
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                    MessageList.Add(GetTwatchMessage(username, msg[(int)Random.Range(0f,(int)msg.Length)]));
+                    break;
+                case 8:
+                case 9:
+                    MessageList.Add(GetTwatchMessage(username, msg[(int)Random.Range(0f,(int)msg.Length)]));
+                    MessageList.Add(GetTwatchMessage(username, msg[(int)Random.Range(0f,(int)msg.Length)]));
+                    break;
+                case 10:
+                    MessageList.Add(GetTwatchMessage(username, msg[(int)Random.Range(0f,(int)msg.Length)]));
+                    MessageList.Add(GetTwatchMessage(username, msg[(int)Random.Range(0f,(int)msg.Length)]));
+                    MessageList.Add(GetTwatchMessage(username, msg[(int)Random.Range(0f,(int)msg.Length)]));
+                    break;
+            }
+            nextMessageTime = Time.time + Random.Range(0.5f, 2f);
         }
     }
 
@@ -48,7 +69,7 @@ public class TwatchChat : MonoBehaviour
         return (int) Random.Range(1f,4f);
     }
 
-    public void SendTwatchMessage(string Username, string TxtInput)
+    public Message GetTwatchMessage(string Username, string TxtInput)
     {
         if (MessageList.Count >= MaxMessages)
         {
@@ -60,8 +81,8 @@ public class TwatchChat : MonoBehaviour
         NewMessage.MsgText = TxtInput;
         TextMeshProUGUI NewTMP = Instantiate(TwatchTextObject,ChatPanel.transform);
         NewMessage.MessageTextObject = NewTMP;
-        NewMessage.MessageTextObject.text = "<color=\"" + colors[(int)Random.Range(0f,(int)colors.Length)] + "\">" + NewMessage.User + "</color>" + ": " + NewMessage.MsgText;
-        MessageList.Add(NewMessage);
+        NewMessage.MessageTextObject.text = "<color=" + colors[(int)Random.Range(0f,(int)colors.Length)] + ">" + NewMessage.User + "</color>" + ": " + NewMessage.MsgText;
+        return NewMessage;
     }
 }
 
